398e3efeb2a9530a1f0ccc1d55c2b2c3bd026cc6
Timoteo <timoteo@hdinfo-ma.com>
2026-02-17 10:25:15 -0300

    feat: correção do model usuario.

--- src/models/usuario_model.py
+++ src/models/usuario_model.py
@@ -9,10 +9,17 @@ class User(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.Datetime, onupdate=datetime.datetime.now)
-    updated_at = db.Column(db.Datetine, server_default=db.func.now())
+    created_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+    updated_at = db.Column(db.DateTine, server_default=db.func.now())
     
     tarefas = db.relationship("Tarefa", back_populates="usuario")
+    
+    def __init__(self, name, email, password_hash, created_at, updated_at):
+        self.name = name
+        self.email = email,
+        self.password_hash = password_hash,
+        self.created_at = created_at
+        self.updated_at = updated_at
     
     def __repr__(self):
         return f"User: {self.name} - {self.email} - {self.password_hash} - {self.created_at} - {self.updated_at}"
