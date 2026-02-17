e6f435e46018aeb0157b04bb432ba54d8defa89c
Timoteo <timoteo@hdinfo-ma.com>
2026-02-17 15:55:27 -0300

    feat: correção do model usuario.

--- src/models/usuario_model.py
+++ src/models/usuario_model.py
@@ -1,4 +1,5 @@
 from src.settings.extensions import db
+from sqlalchemy import func
 import datetime
 
 class User(db.Model):
@@ -9,17 +10,15 @@ class User(db.Model):
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
-    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now)
     
     tarefas = db.relationship("Tarefa", back_populates="usuario")
     
-    def __init__(self, name, email, password_hash, created_at, updated_at):
+    def __init__(self, name, email, password_hash):
         self.name = name
         self.email = email,
         self.password_hash = password_hash,
-        self.created_at = created_at
-        self.updated_at = updated_at
     
     def __repr__(self):
         return f"User: {self.name} - {self.email} - {self.password_hash} - {self.created_at} - {self.updated_at}"
